ab0556bcff9420fbbf19a2ea7ce3517bea37e7fe
yudou <1744547395@qq.com>
2021-09-23 15:09:44 +0800

    admin提交

---
+++ Admin.java
@@ -0,0 +1,32 @@
+package com.ambow.entity;
+
+public class Admin {
+
+    private Integer a_id;
+    private String a_account;
+    private String a_pwd;
+
+    public Integer getA_id() {
+        return a_id;
+    }
+
+    public void setA_id(Integer a_id) {
+        this.a_id = a_id;
+    }
+
+    public String getA_account() {
+        return a_account;
+    }
+
+    public void setA_account(String a_account) {
+        this.a_account = a_account;
+    }
+
+    public String getA_pwd() {
+        return a_pwd;
+    }
+
+    public void setA_pwd(String a_pwd) {
+        this.a_pwd = a_pwd;
+    }
+}
